254c894a897f0f18a8448914d84238ca2458088a
Josebratt <josebratt@gmail.com>
2021-11-11 10:58:48 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDKxKvUVU2qCVmcZy5oi5uTPG1ncNegaq8",
+    authDomain: "log-in-dashboard.firebaseapp.com",
+    projectId: "log-in-dashboard",
+    storageBucket: "log-in-dashboard.appspot.com",
+    messagingSenderId: "480512035802",
+    appId: "1:480512035802:web:7c5c91cc3e1db1d1b10ee3"
+  }
 };
